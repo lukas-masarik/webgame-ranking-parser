d54b2bf054893a7a5cce3c04c20a1cfd4c2fe759
Lukáš Masařík <masi2046@gmail.com>
2021-10-01 00:20:06 +0200

    Simplify reading process from input

--- src/main/kotlin/services/inputreaders/SimpleInputReader.kt
+++ src/main/kotlin/services/inputreaders/SimpleInputReader.kt
@@ -1,21 +1,14 @@
 package services.inputreaders
 
-import enums.EProcessorType
 import services.inputreaders.api.InputReader
 import services.processors.WinnersByPrestigeOrAreaProcessor
 import services.processors.ParseOnlyProcessor
 import services.processors.api.Processor
-import java.lang.IllegalArgumentException
 import kotlin.system.exitProcess
 
 class SimpleInputReader : InputReader {
 
     override fun selectProcessorFromInput(): Processor {
-        val input = askForInput()
-        return extractProcessor(input)
-    }
-
-    private fun askForInput(): String? {
         print(
             """
                 Available processes:
@@ -26,23 +19,17 @@ class SimpleInputReader : InputReader {
                 Choose processor: 
             """.trimIndent()
         )
-        return readLine()
+        val input = readLine()
+        return extractProcessorFromInput(input)
     }
 
-    private fun extractProcessor(input: String?): Processor {
+    private fun extractProcessorFromInput(input: String?): Processor {
         if (input?.toIntOrNull() == null) selectProcessorFromInput()
 
-        val processorType = try {
-            EProcessorType.fromNumber(input!!.toInt())
-        } catch (e: IllegalArgumentException) {
-            null
-        }
-        if (processorType == null) selectProcessorFromInput()
-
-        return when (processorType) {
-            EProcessorType.EXIT -> exitProcess(1)
-            EProcessorType.PARSE_ONLY -> ParseOnlyProcessor()
-            EProcessorType.WINNERS_BY_PRESTIGE_OR_AREA -> WinnersByPrestigeOrAreaProcessor()
+        return when (input!!.toInt()) {
+            0 -> exitProcess(1)
+            1 -> ParseOnlyProcessor()
+            2 -> WinnersByPrestigeOrAreaProcessor()
             else -> selectProcessorFromInput()
         }
     }
